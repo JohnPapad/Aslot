0d0198c60be58fdf7eb4660cf3d384e9c030f263
NedicNemanja <sdi1400123@di.uoa.gr>
2020-04-04 01:07:56 +0300

    added stores to installed apps

--- backend/src/stores/admin.py
+++ backend/src/stores/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import *
+
+print("GAMW TI MANA SOU")
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = (
+        'address',
+        'store_type',
+        'name',
+        'email',
+        'opening_from_hour',
+        'opening_to_hour',
+        'persons_per_slot',
+        'time_slot_duration',
+        'lat',
+        'lng'
+    )
+
+class OpeningHoursAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
+admin.site.register(Store, StoreAdmin)
+admin.site.register(OpeningHours)
