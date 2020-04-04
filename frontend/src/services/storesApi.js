e1ed58cf18986bc9c2b38939be58cf3ed72028f9
yannis-b450 <sdi1400088@di.uoa.gr>
2020-04-04 04:43:24 +0300

    axios

--- frontend/src/services/storesApi.js
+++ frontend/src/services/storesApi.js
@@ -1,3 +1,4 @@
+import Layout from "../hoc/Layout/Layout";
 
 export const storesApi = {
     getPins
@@ -7,6 +8,19 @@ export const storesApi = {
 function getPins(axios)
 {
     return axios.get('/stores/pins')
+        .then( response =>  response ? response.data : null)
+        .catch( err => err);
+}
+
+// JsonRequest:
+// {
+//     searchTerm: "   ",
+//     lat,
+//     lng
+// }
+function getStores(axios, jsonRequest)
+{
+    return axios.get('/stores', jsonRequest)
         .then( response =>  response ? response.data : null)
         .catch( err => err);
 }
