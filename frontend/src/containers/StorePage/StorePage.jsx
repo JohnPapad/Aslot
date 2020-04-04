2d6a83e2d0c5583b5c5e5c2a1944a77c3c082557
John Pap <sdi1400144@di.uoa.gr>
2020-04-04 05:18:55 +0300

    Starting designing the StorePage

--- frontend/src/containers/StorePage/StorePage.jsx
+++ frontend/src/containers/StorePage/StorePage.jsx
@@ -2,14 +2,59 @@ import React, { useState } from 'react';
 
 import classes from './StorePage.module.scss';
 
-import { Container, Row, Button } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 import TimeSlotModal from '../../components/TimeSlotModal/TimeSlotModal';
 
 export default function StorePage() {
 
 
     return (
-        <Container id={classes.content}>
+        <Container fluid id={classes.content} className="border">
+
+            <Row>
+                <Col xs="8" className="border">
+
+
+                </Col>
+
+                <Col xs="4" className="border">
+
+
+                </Col>
+            </Row>
+
+            <Row className="mb-5">
+                <div class="d-flex align-items-stretch">
+                    <div className="border p-3">
+                        <div>
+                            Χειρουργικες Μασκες
+                        </div>
+                        <div>
+                            Διαθεσιμα τεμαχια: 10
+                        </div>
+                        <div>
+                            <Button>
+                                Κρατησησ
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div className="border">
+                        <div>
+                            Χειρουργικες Μασκες
+                        </div>
+                        <div>
+                            Διαθεσιμα τεμαχια: 10
+                        </div>
+                        <div>
+                            <Button>
+                                Κρατησησ
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Row>
+
             <TimeSlotModal />
         </Container>
     );
