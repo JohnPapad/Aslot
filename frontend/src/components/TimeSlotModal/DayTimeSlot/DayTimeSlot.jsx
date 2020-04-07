5082572d57e809c83d6f4cd267ab97215fa0c085
John Pap <sdi1400144@di.uoa.gr>
2020-04-07 06:06:15 +0300

    Added highlight for selected time slot in DayTimeSlot

--- frontend/src/components/TimeSlotModal/DayTimeSlot/DayTimeSlot.jsx
+++ frontend/src/components/TimeSlotModal/DayTimeSlot/DayTimeSlot.jsx
@@ -8,9 +8,15 @@ const dummyTimeSlots = [ "08:00-09:00", "18:00-19:00", "10:00-20:00" ]
 
 export default function DayTimeSlot(props) {
 
+    let selectedDay = false;
+    if (props.selectedTimeSlot)
+    {
+        selectedDay = props.day === props.selectedTimeSlot.day;
+    }
+
     const timeSlots = dummyTimeSlots.map( (timeSlot, i) =>
         <Row className="border" key={i}>
-            <Col className={classes.slot + " p-0 d-flex justify-content-center font-weight-bold"} onClick={()=>props.slotClickedHandler(props.day, timeSlot)}>
+            <Col id={selectedDay && timeSlot === props.selectedTimeSlot.timeSlot  ? classes.selected: ""} className={classes.slot + " p-0 d-flex justify-content-center font-weight-bold"} onClick={()=>props.slotClickedHandler(props.day, timeSlot)}>
                 {timeSlot}
             </Col>
         </Row>
