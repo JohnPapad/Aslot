64aa6cad2a937905b10ddf88766ebf0c74431bfd
John Pap <giannispapad96@gmail.com>
2021-03-26 23:28:07 +0200

    Removed unnecessary code

--- backend_for_reference/src/HackApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User ,Report, Comment
-
-admin.site.register(User)
-admin.site.register(Report)
-admin.site.register(Comment)
